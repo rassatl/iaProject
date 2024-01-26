2f94852bda469452169ce496e8a4d6dff05b5e00
Lou RASSAT <lou.rassat@etu.univ-smb.fr>
2024-01-26 10:25:51 +0100

    Initial Commit

---
+++ iaProject/iaProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  iaProjectApp.swift
+//  iaProject
+//
+//  Created by Lou RASSAT on 1/26/24.
+//
+
+import SwiftUI
+
+@main
+struct iaProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

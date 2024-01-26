83176f57f54cf82826670f321e3f33235c901617
Lou RASSAT <lou.rassat@etu.univ-smb.fr>
2024-01-26 11:47:39 +0100

    push ia project

--- iaProject/ContentView.swift
+++ iaProject/ContentView.swift
@@ -7,18 +7,118 @@
 
 import SwiftUI
 
-struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+enum Sentiment: String {
+    case positive = "POSITIVE"
+    case negative = "NEGATIVE"
+    case mixed = "MIXED"
+    case neutral = "NEUTRAL"
+}
+
+extension Sentiment {
+    func getColor() -> Color {
+        switch self{
+        case .positive:
+            return .green
+        case .negative:
+            return .red
+        case .mixed:
+            return .purple
+        case .neutral:
+            return .gray
         }
-        .padding()
+    }
+ }
+
+extension Sentiment {
+    func getEmoji() -> String {
+        switch self{
+        case .positive:
+            return "😃"
+        case .negative:
+            return "😔"
+        case .mixed:
+            return "😕"
+        case .neutral:
+            return "😐"
+        }
+    }
+ }
+
+struct ContentView: View {
+    @State private var sentimentText: String = ""
+    @State private var outputSentiment: Sentiment?
+    
+    var body: some View {
+        NavigationStack {
+            VStack{
+                VStack {
+                    Text("Entrez une phrase, l'IA va deviner votre sentiment ")
+                        .font(.system(size: 20))
+                        .lineLimit(2)
+                        .font(.title)
+                        .multilineTextAlignment(.leading)
+                        .padding()
+                    
+                    TextEditor(text: $sentimentText)
+                        .frame(height: 100)
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                        .foregroundColor(.black)
+                        .cornerRadius(15)
+                        .padding()
+                        .onChange(of: sentimentText) { oldValue, newValue in
+                            outputSentiment = nil
+                        }
+                        
+
+                    Button(action: {
+                        guessSentiment()
+                    }) {
+                        Text("Devinez le sentiment")
+                            .padding()
+                            .background(.blue)
+                            .cornerRadius(10)
+                    }
+                }
+                .padding()
+                .background(.purple)
+                .cornerRadius(15)
+                .foregroundStyle(.white)
+            }
+            .navigationTitle("🧠 IA du futur")
+            .padding()
+            VStack{
+                Text(outputSentiment?.getEmoji() ?? "")
+                    .font(.system(size: 40))
+                    .multilineTextAlignment(.center)
+                    .padding()
+                
+                Text(outputSentiment?.rawValue ?? "")
+                    .font(.system(size: 40, weight: .bold))
+                    .font(.title)
+                    .foregroundColor(.black)
+                    .multilineTextAlignment(.center)
+                    .padding()
+            }
+            .background(outputSentiment?.getColor() ?? .white)
+            .cornerRadius(15)
+        }
+    }
+
+    
+    func guessSentiment() {
+        do {
+            // MyModel est une classe générée automatiquement par Xcode
+            let model = try Sentimentiutacy_1(configuration: .init())
+            let prediction = try model.prediction(text: sentimentText)
+            outputSentiment = Sentiment(rawValue: prediction.label.uppercased())
+        } catch {
+            print("Something went wrong")
+        }
     }
 }
 
-#Preview {
-    ContentView()
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
 }
